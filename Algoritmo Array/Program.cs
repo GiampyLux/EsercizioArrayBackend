e5da2bd61a28a6e2d4fc0a43e3d9cef8325b0f0f
GiampyLux <giampaolopaladino01@gmail.com>
2024-06-19 11:42:12 +0200

    Aggiungere i file di progetto.

---
+++ Algoritmo Array/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Algoritmo_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string[] listanomi = { "Flavio", "Giuseppe", "Sara", "chiara", "lorenzo" };
+                string nome;
+                int i;
+                bool verifica = false;
+
+                Console.WriteLine("Inserire il nome da trovare");
+                nome = Console.ReadLine();
+
+                for (i = 0; i < listanomi.Length; i++)
+                {
+                    if (listanomi[i] == nome)
+                    {
+                        Console.WriteLine("Il nome è presente nella lista");
+                        verifica = true;
+                    }
+
+                }
+                if (verifica == false)
+                {
+                    Console.WriteLine("Il nome non è presente nella lista");
+                }
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ Lunghezza Array/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Lunghezza_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            Console.WriteLine("Specifica la lunghezza dell'array");
+            x = int.Parse(Console.ReadLine());
+            int[] numList = new int[x];
+            int i;
+            int add = 0;
+            for (i = 0; i < numList.Length; i++)
+            {
+                Console.WriteLine("inserisci un numero");
+                numList[i] = int.Parse(Console.ReadLine());
+            }
+            for (i = 0; i < numList.Length; i++)
+            {
+                add += numList[i];
+            }
+            Console.WriteLine($"\nLa somma è: {add}\n");
+            Console.WriteLine($"La media è: {add
+                / x}");
+            Console.ReadLine();
+        }
+    }
+}
